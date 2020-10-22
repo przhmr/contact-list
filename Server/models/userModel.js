e61dfe6222c0ae9a282ca44cb08f01216b99c709
Hmr <hmr@pop-os.localdomain>
2020-10-22 19:04:12 -0400

    120 mins

---
+++ Server/models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+email:{
+    type: String,
+    required: true,
+    unique: true
+},
+
+
+password:{
+    type: String,
+    required: true,
+ 
+},
+
+lastName:{
+ type: String,
+
+},
+
+firstName:{
+    type: String,
+   
+   },
+
+
+
+dateCreated: {
+
+type: Date,
+
+},
+
+
+
+
+
+});
+
+module.exports= User = mongoose.model("user", userSchema)
